585190c7603236bf9fc0ab406636a7ec6ff875ae
logonpf <logonpf@fiap.com.br>
2018-04-20 10:31:29 -0300

    JSF e WS Resfutl

--- 14-JSF/src/br/com/fiap/jsf/bean/MedicoBean.java
+++ 14-JSF/src/br/com/fiap/jsf/bean/MedicoBean.java
@@ -24,6 +24,19 @@ public class MedicoBean {
 		service = new MedicoService();
 	}
 	
+	//Método do clique do botão excluir da listagem
+	public void excluir(int codigo) {
+		FacesMessage msg;
+		try {
+			service.remover(codigo);
+			msg = new FacesMessage("Removido!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			msg = new FacesMessage("Erro...");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
 	//Lista de médico para a tabela
 	public List<Medico> getMedicos(){
 		try {
@@ -34,18 +47,28 @@ public class MedicoBean {
 		return new ArrayList<Medico>();
 	}
 	
-	public void cadastrar() {
+	//Método do clique do botão da tela 
+	public String salvar() {
 		FacesMessage msg;
 		try {
-			service.cadastrar(medico);
-			msg = new FacesMessage("Cadastrado!");
+			//Verificar se é para cadastrar ou atualizar
+			if (medico.getId() == 0) {
+				service.cadastrar(medico);
+				msg = new FacesMessage("Cadastrado!");
+			}else {
+				service.atualizar(medico);
+				msg = new FacesMessage("Atualizado!");
+				FacesContext.getCurrentInstance().addMessage(null, msg);
+				return "lista-medicos"; //nome do arquivo da página
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			msg = new FacesMessage("Erro");
 		}
 		//Adiciona a mensagem para a tela
 		FacesContext.getCurrentInstance().addMessage(null, msg);
-	}
+		return "medico"; // nome do arquivo da página xhtml
+	} 
 
 	public Medico getMedico() {
 		return medico;
